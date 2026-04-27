f6128a3ba7ae465cb6e1d7c00da98f21550fc3ae
raj <rajsharma2278608@gmail.com>
2026-04-27 13:27:01 +0530

    room rep query

--- src/main/java/com/tech_raj/repo/RoomRepo.java
+++ src/main/java/com/tech_raj/repo/RoomRepo.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -13,5 +14,11 @@ public interface RoomRepo extends JpaRepository<Room,Long> {
     @Query("SELECT distinct r.roomType FROM Room AS r ")
     List<String>findDistinctByRoomTypes();
 
+
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomTypes% AND r.id NOT IN (SELECT bk.room FROM Booking  bk)")
+    List<Room>findAvailableRoomByDatesAndTypes(LocalDate checkInDate, LocalDate checkOutDate, String roomTypes);
+
+
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN(SELECT b.room FROM Booking b)")
     List<Room>findAllAvailableRoom();
 }
